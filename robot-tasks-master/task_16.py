cc0f7d59eb138d5dfa7b2b2bd26d224903f1343a
Vasily Komyagin <komyagin@gmail.com>
2022-09-02 23:54:34 +0300

    fix robot test16

--- robot-tasks-master/task_16.py
+++ robot-tasks-master/task_16.py
@@ -8,11 +8,11 @@ def task_8_22():
     while not wall_is_above():
         move_up()
     if wall_is_on_the_left():
-        while not wall_is_on_the_left():
-            move_left()
-    else:
         while not wall_is_on_the_right():
             move_right()
+    else:
+        while not wall_is_on_the_left():
+            move_left()
 
 
 if __name__ == '__main__':
