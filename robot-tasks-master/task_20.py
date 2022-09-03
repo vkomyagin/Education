48d6409b6a14b36a43ce7c2a3981749fc757f960
Vasily Komyagin <komyagin@gmail.com>
2022-09-03 23:42:11 +0300

    tasks 20-21

--- robot-tasks-master/task_20.py
+++ robot-tasks-master/task_20.py
@@ -5,8 +5,20 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
-
+    horizontal_size = 27
+    vertical_size = 12
+    move_right()
+    for __ in range(vertical_size // 2):
+        for _ in range(horizontal_size):
+            fill_cell()
+            move_right()
+        move_left()
+        move_down()
+        for _ in range(horizontal_size):
+            fill_cell()
+            move_left()
+        move_right()
+        move_down()
 
 if __name__ == '__main__':
     run_tasks()
--- robot-tasks-master/task_21.py
+++ robot-tasks-master/task_21.py
@@ -5,8 +5,16 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
-
+    stair_size = 13
+    move_down()
+    move_right()
+    for step in range(stair_size):
+        for _ in range(step + 1):
+            fill_cell()
+            move_right()
+        for _ in range(step + 1):
+            move_left()
+        move_down()
 
 if __name__ == '__main__':
     run_tasks()
