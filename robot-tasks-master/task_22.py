3594f2c4b6c1dbf8f1102f58500ba38a84ac391f
Vasily Komyagin <komyagin@gmail.com>
2022-09-04 12:43:50 +0300

    task 22

--- robot-tasks-master/task_22.py
+++ robot-tasks-master/task_22.py
@@ -5,7 +5,27 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+    flag = True
+    while flag:
+        while True:
+            fill_cell()
+            if wall_is_on_the_right():
+                break
+            move_right()
+        if not wall_is_beneath():
+            move_down()
+        else:
+            flag = False
+        while True:
+            if flag:
+                fill_cell()
+            if wall_is_on_the_left():
+                break
+            move_left()
+        if not wall_is_beneath():
+            move_down()
+        else:
+            flag = False
 
 
 if __name__ == '__main__':
