c605acbdfa61fe9aa49a5436387f531c8408dca4
Vasily Komyagin <komyagin@gmail.com>
2022-09-04 12:29:45 +0300

    optimise task 21

--- robot-tasks-master/task_21.py
+++ robot-tasks-master/task_21.py
@@ -8,13 +8,18 @@ def task_4_11():
     stair_size = 13
     move_down()
     move_right()
-    for step in range(stair_size):
+    for step in range(0, stair_size, 2):
         for _ in range(step + 1):
             fill_cell()
             move_right()
-        for _ in range(step + 1):
-            move_left()
         move_down()
+        for _ in range(step + 2):
+            if step + 2 < stair_size:
+                fill_cell()
+            move_left()
+        if step + 2 < stair_size:
+            move_down()
+        move_right()
 
 if __name__ == '__main__':
     run_tasks()
