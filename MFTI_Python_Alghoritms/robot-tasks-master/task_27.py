d3337828974deab07acc587e5762efb66449ef9f
Vasily Komyagin <komyagin@gmail.com>
2022-09-11 15:18:42 +0300

    add 27

--- MFTI_Python_Alghoritms/robot-tasks-master/task_27.py
+++ MFTI_Python_Alghoritms/robot-tasks-master/task_27.py
@@ -5,8 +5,18 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
-
+    steps = 1
+    move_right()
+    while True:
+        fill_cell()
+        for _ in range(steps):
+            if wall_is_on_the_right():
+                break
+            else:
+                move_right()
+        steps +=1
+        if wall_is_on_the_right():
+            break
 
 if __name__ == '__main__':
     run_tasks()
