1d259f9ec5c992910e7062f458d8e3bb3ff48a6a
Vasily Komyagin <komyagin@gmail.com>
2022-09-11 19:41:36 +0300

    add 31

--- MFTI_Python_Alghoritms/robot-tasks-master/task_31.py
+++ MFTI_Python_Alghoritms/robot-tasks-master/task_31.py
@@ -3,10 +3,24 @@
 from pyrob.api import *
 
 
-@task(delay=0.01)
+@task(delay=0.05)
 def task_8_30():
-    pass
-
+    turn_flag = False
+    while True:
+        if not wall_is_beneath():
+            number_of_turns = 0
+            turn_flag = False
+            move_down()
+        else:
+            if turn_flag:
+                if wall_is_on_the_left():
+                    break
+                else:
+                    move_left()
+            elif wall_is_on_the_right():
+                turn_flag = True
+            else:
+                move_right()
 
 if __name__ == '__main__':
     run_tasks()
