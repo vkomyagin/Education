35fe5f82da954584216d3f945f11b8fdff55a500
vas <komyagin@gmail.com>
2024-02-24 19:35:24 +0300

    lab 8 3-6

---
+++ MFTI_Python_Alghoritms/Lab8/Task_7.py
@@ -0,0 +1,33 @@
+"""Кривая дракона"""
+
+import turtle
+import time
+
+=======
+def draw(l, n):
+    # print(n)
+    if n == 0:
+        turtle.right(45)
+        turtle.forward(l)
+        turtle.left(90)
+        turtle.forward(l)
+        turtle.left(45)
+        return
+
+    x = (l ** 2 / 2) ** 0.5
+
+    turtle.left(45)
+    draw(x, n - 1)
+    turtle.right(90)
+    draw(x, n - 1)
+    turtle.left(45)
+
+    # time.sleep(2)
+
+
+if __name__ == '__main__':
+    turtle.pendown()
+    turtle.speed('fastest')
+    draw(70, 8)
+    print('Done')
+    time.sleep(100)
